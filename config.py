29628b70d704d790557d995c976c1ba08c339e8b
xishan Dong <100206449+xishandong@users.noreply.github.com>
2023-06-20 12:52:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # 加密cookie的key
-SECRET_KEY = 'dxsshidashuaibihhh'
+SECRET_KEY = ''
 
 
 # 数据库的配置
 # MySQL的主机名
-HOSTNAME = "113.125.109.60"
+HOSTNAME = ""
 # MySQL监听的端口, 默认3306
 PORT = 3306
 # 连接MySQL的用户名
 USERNAME = "root"
 # 连接MySQL的密码
-PASSWORD = "!d0x9sDXS"
+PASSWORD = ""
 # MySQL上连接的数据库名称
-DATABASE = "nlp_final"
+DATABASE = ""
 DB_URI = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8'
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
